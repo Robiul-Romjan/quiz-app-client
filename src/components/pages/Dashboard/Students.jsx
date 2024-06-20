e31a130c367a5564acdd24b228d55fb860d20506
robiul-romjan <robiulromjan7@gmail.com>
2024-06-20 23:50:17 +0600

    add search

--- src/components/pages/Dashboard/Students.jsx
+++ src/components/pages/Dashboard/Students.jsx
@@ -1,64 +1,98 @@
 import { useEffect, useState } from "react";
-import Loader from "../../shared/Loader";
+import useDebounce from "../../hooks/useDebounce";
 
 const Students = () => {
-  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const debouncedSearchQuery = useDebounce(searchQuery, 500);
 
   useEffect(() => {
-    setLoading(true);
-    fetch("http://localhost:5000/users/students")
-      .then((res) => res.json())
-      .then((data) => setUsers(data))
-      .finally(() => setLoading(false));
-  }, []);
+    const fetchUsers = async () => {
+      setError("");
+      try {
+        let url = "http://localhost:5000/searchUser";
+        if (debouncedSearchQuery) {
+          const queryParam = isNaN(debouncedSearchQuery)
+            ? `name=${debouncedSearchQuery}`
+            : `id=${debouncedSearchQuery}`;
+          url += `?${queryParam}`;
+        }
+        const response = await fetch(url);
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message || "Error fetching users");
+        }
+        setUsers(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
 
-  if (loading) {
-    return (
-      <div className="w-full h-[100vh] flex justify-center items-center">
-        <Loader />
+    fetchUsers();
+  }, [debouncedSearchQuery]);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value.trim());
+  };
+
+  return (
+    <div className="w-[90%] mx-auto mt-12">
+      <h2 className="text-2xl md:text-2xl font-semibold text-center text-black">
+        All Students
+      </h2>
+      <div className="flex items-center justify-center gap-2">
+        <div className="h-1 w-36 bg-green-500"></div>
+        <span className="text-red-500 font-semibold">Manage Students</span>
+        <div className="h-1 w-36 bg-green-500"></div>
       </div>
-    );
-  }
 
-  return <div className="w-[90%] mx-auto mt-12">
-  <h2 className="text-2xl md:text-2xl font-semibold text-center text-black">
-    All Students
-  </h2>
-  <div className="flex items-center justify-center gap-2">
-    <div className="h-1 w-36 bg-green-500"></div>
-    <span className="text-red-500 font-semibold">Manage Students</span>
-    <div className="h-1 w-36 bg-green-500"></div>
-  </div>
+      <div className="flex justify-center mt-6">
+        <input
+          type="search"
+          className="border-2 border-gray-400 p-2 rounded w-1/2"
+          placeholder="Search by Id or Name"
+          aria-label="Search"
+          aria-describedby="button-addon1"
+          onChange={handleSearchChange}
+        />
+      </div>
 
-  {
-    <div className="overflow-x-auto mt-8">
-      <table className="table text-black">
-        {/* head */}
-        <thead>
-          <tr className="text-gray-500 font-semibold text-sm">
-            <th>#</th>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Department</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, i) => (
-            <tr key={user._id}>
-              <th>{i + 1}</th>
-              <td>{user?.id}</td>
-              <td>{user?.name}</td>
-              <td>{user?.email}</td>
-              <td>{user?.department}</td>
+      <div className="overflow-x-auto mt-8">
+        <table className="table text-black">
+          <thead>
+            <tr className="text-gray-500 font-semibold text-sm">
+              <th>#</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Department</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {error ? (
+              <tr>
+                <td colSpan="5" className="text-center mt-8 font-bold">
+                  {error}
+                </td>
+              </tr>
+            ) : (
+              users.map((user, i) => (
+                <tr key={user._id}>
+                  <th>{i + 1}</th>
+                  <td>{user?.id}</td>
+                  <td>{user?.name}</td>
+                  <td>{user?.email}</td>
+                  <td>{user?.department}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-  }
-</div>;
+  );
 };
 
 export default Students;
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,12 +1,21 @@
 import { useContext, useEffect, useState } from "react";
-import { FaBars, FaBookmark, FaCheck, FaHome, FaUser, FaUtensils } from "react-icons/fa";
+import {
+  FaBars,
+  FaBookmark,
+  FaCheck,
+  FaHome,
+  FaUser,
+  FaUtensils,
+} from "react-icons/fa";
 import { Link, NavLink, Outlet } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import { ImProfile } from "react-icons/im";
 import Loader from "../components/shared/Loader";
+import { IoExitOutline } from "react-icons/io5";
+
 
 const DashboardLayout = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
@@ -16,12 +25,21 @@ const DashboardLayout = () => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => {
-        const studentRole = data.find((student) => student?.email === user?.email);
+        const studentRole = data.find(
+          (student) => student?.email === user?.email
+        );
         setCurrentUser(studentRole);
       })
-      .catch((error) => setError(`Something went wrong, ${error.message}. Try again later`))
+      .catch((error) =>
+        setError(`Something went wrong, ${error.message}. Try again later`)
+      )
       .finally(() => setLoading(false));
   }, [user?.email]);
+
+
+  const handleLogout = () => {
+    logOut();
+  };
 
   if (loading) {
     return (
@@ -42,7 +60,7 @@ const DashboardLayout = () => {
         >
           <FaBars size={24} />
         </label>
-        
+
         {/* Page content here */}
         {error ? (
           <div className="w-full h-[90vh] flex items-center justify-center text-red-500 text-2xl">
@@ -52,7 +70,7 @@ const DashboardLayout = () => {
           <Outlet />
         )}
       </div>
-      
+
       <div className="drawer-side min-h-screen bg-gray-800 text-white">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="p-4 w-80 h-full">
@@ -64,7 +82,10 @@ const DashboardLayout = () => {
           </div>
 
           <li className="mb-4">
-            <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard">
+            <NavLink
+              className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+              to="/dashboard"
+            >
               <ImProfile /> Profile
             </NavLink>
           </li>
@@ -72,12 +93,18 @@ const DashboardLayout = () => {
           {currentUser?.role === "Admin" && (
             <div>
               <li className="mb-4">
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/manage-users">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/manage-users"
+                >
                   <FaUser /> Manage Users
                 </NavLink>
               </li>
               <li>
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/manage-quiz">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/manage-quiz"
+                >
                   <FaBars /> Manage Quiz
                 </NavLink>
               </li>
@@ -87,17 +114,26 @@ const DashboardLayout = () => {
           {currentUser?.role === "Teacher" && (
             <div>
               <li className="mb-4">
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/students">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/students"
+                >
                   <FaUser /> All Students
                 </NavLink>
               </li>
               <li className="mb-4">
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/add-question">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/add-question"
+                >
                   <FaUtensils /> Add Question
                 </NavLink>
               </li>
               <li>
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/my-quizzes">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/my-quizzes"
+                >
                   <FaCheck /> My Quizzes
                 </NavLink>
               </li>
@@ -107,17 +143,34 @@ const DashboardLayout = () => {
           {currentUser?.role === "Student" && (
             <div>
               <li className="mb-4">
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/exams">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/exams"
+                >
                   <FaBookmark /> Exams
                 </NavLink>
               </li>
               <li>
-                <NavLink className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors" to="/dashboard/quiz-results">
+                <NavLink
+                  className="flex items-center gap-2 px-4 py-2 rounded bg-gray-700 hover:bg-gray-600 transition-colors"
+                  to="/dashboard/quiz-results"
+                >
                   <FaBookmark /> Quiz Result & Summary
                 </NavLink>
               </li>
             </div>
           )}
+          <div className="mt-20">
+            <li className="mb-4">
+              <button
+              onClick={handleLogout}
+                className="flex items-center gap-2 px-4 py-2 rounded bg-red-500 hover:bg-gray-600 transition-colors"
+                
+              >
+                <IoExitOutline  size={25}/> Log out
+              </button>
+            </li>
+          </div>
         </ul>
       </div>
     </div>
